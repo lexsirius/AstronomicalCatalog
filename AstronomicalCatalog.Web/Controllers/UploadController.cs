46fd6ecf49fa621646cd255f375e4e0b93e888c4
lexsirius <abry2@mail.ru>
2018-05-05 18:59:32 +0500

    Added web site and ability to view files on Web

---
+++ AstronomicalCatalog.Web/Controllers/UploadController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AstronomicalCatalogLibrary;
+
+namespace AstronomicalCatalog.Web.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Print(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var star = Serializer.LoadFromStream(file.InputStream);
+                return View(star);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
